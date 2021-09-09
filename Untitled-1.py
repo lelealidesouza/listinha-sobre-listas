ed3db7c55e74ad68e207fb31c93a9132d364b580
lelealidesouza <lelealidesouza@gmail.com>
2021-09-09 14:31:15 -0300

    atividade 1 e 2
    atividade da lista de atividade de listas

---
+++ Untitled-1.py
@@ -0,0 +1,7 @@
+num1 = int(input("Um numero inteiro: "))
+num2 = int(input("Um numero inteiro: "))
+num3 = int(input("Um numero inteiro: "))
+num4 = int(input("Um numero inteiro: "))
+num5 = int(input("Um numero inteiro: "))
+lista = [num1, num2, num3, num4, num5]
+print(lista)
---
+++ atividade-2.py
@@ -0,0 +1,8 @@
+num1 = float(input("Um numero inteiro: "))
+num2 = float(input("Um numero inteiro: "))
+num3 = float(input("Um numero inteiro: "))
+num4 = float(input("Um numero inteiro: "))
+num5 = float(input("Um numero inteiro: "))
+lista = [num1, num2, num3, num4, num5]
+print(lista)
+print("minha lista com as posições invertidas ",(list(reversed(lista_copia)))
