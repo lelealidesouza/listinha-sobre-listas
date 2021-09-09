d129e837e7bf847d03b8b5e5f940fc29556e1f52
lelealidesouza <lelealidesouza@gmail.com>
2021-09-09 15:01:38 -0300

    atividade-5

---
+++ atividade-5.py
@@ -0,0 +1,10 @@
+lista = ['BANANA', 'MAÇA', 'LARANJA', 'MORANGO'] 
+print(lista)
+
+per = input("digite para conferir se esta na lista: ")
+
+if per in lista:
+    print("Sim, está na lista!")
+else:
+    print("Não está na lista")
+    
